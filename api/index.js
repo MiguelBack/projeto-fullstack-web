12b31a207edb658a53b1401aee0ff83feab004e7
igor-barros-da-silva <barrosigor74@gmail.com>
2022-06-20 07:12:10 -0300

    Conexão com o banco de dados.

---
+++ api/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+// Conexão com o banco de dados
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/teste') //{ useMongoClient: true })
+    .then(() => {
+        console.log("++ Conexão realizada com sucesso!")
+    })
+    .catch(err => console.log(err));
