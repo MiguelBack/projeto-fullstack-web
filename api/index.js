c99cbe7b11ed8fae78903fc70ec7dd7df7931d75
igor-barros-da-silva <barrosigor74@gmail.com>
2022-06-22 02:17:13 -0300

    Criação do servidor na porta 3800

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
+var port = 3800;
 
 // Conexão com o banco de dados
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/teste') //{ useMongoClient: true })
     .then(() => {
         console.log("++ Conexão realizada com sucesso!")
+
+        // Criação do servidor
+        app.listen(port, () => {
+            console.log("Servidor em funcionamento: http://localhost:3800")
+        });
     })
     .catch(err => console.log(err));
