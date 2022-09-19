b256e571867fb816dafa223cdc0ffb374cd87550
igor-barros-da-silva <barrosigor74@gmail.com>
2022-09-19 16:37:33 -0300

    Importações  e declarações do componentes no módulo app.

--- front/app/src/app/app.module.ts
+++ front/app/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// COMPONENTES
 import { AppComponent } from './app.component';
+import { LoginComponent } from 'componentes/login/login.component';
+import { RegistroComponent } from 'componentes/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
