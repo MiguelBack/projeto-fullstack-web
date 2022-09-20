1e1e01384937eddf49e896e98dffdc8b77c19da5
igor-barros-da-silva <barrosigor74@gmail.com>
2022-09-19 23:28:58 -0300

    Implementação do componente para acesso a página de cadastro.

--- front/app/componentes/cadastro/cadastro.component.ts
+++ front/app/componentes/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { Usuario } from "modelo/usuario";
 @Component({
   selector: 'cadastro',
   templateUrl: './cadastro.Component.html'
@@ -8,9 +9,26 @@ import { Component, OnInit } from "@angular/core";
 export class CadastroComponent implements OnInit{
 
   public title: string;
+  // DECLARAÇÃO DO OBJETO USUÁRIO
+  public usuario: Usuario;
 
-  constructor(){
+  constructor(
+    // CONFIGURAÇÃO DAS PROPRIEDADES DAS ROTAS PARA DEFINIR AS CLASSES
+    private _rota: ActivatedRoute,
+    private _rotas: Router
+  ){
     this.title = 'Cadastre-se';
+    // DECLARAÇÃO DE PROPRIEDADE VAZIA, PARA RECEBER VALORES DO FORMULÁRIO
+    this.usuario = new Usuario(
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "FUNCAO_USUARIO",
+      ""  
+    )
   }
 
   ngOnInit(): void {
